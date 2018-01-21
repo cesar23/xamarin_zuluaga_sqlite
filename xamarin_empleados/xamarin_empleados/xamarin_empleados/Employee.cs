55e06f717e8718e2775b0a7b3d76b131cbe44f79
cesar <solito_203@hhotmail.com>
2018-01-20 22:52:06 -0500

    Parte 3/8 - Xamarin.Forms - SQLite

---
+++ xamarin_empleados/xamarin_empleados/xamarin_empleados/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite.Net.Attributes;
+
+namespace xamarin_empleados
+{
+   public  class Employee
+    {
+        [PrimaryKey, AutoIncrement]
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime ContractDate { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool Active { get; set; }
+
+        public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3} {4}", EmployeeId, FirstName, LastName, ContractDate, Salary);
+            return base.ToString();
+        }
+
+    }
+}
---
+++ xamarin_empleados/xamarin_empleados/xamarin_empleados/IConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using SQLite.Net.Interop;
+
+namespace xamarin_empleados
+{
+    public interface IConfig
+    {
+       string DirectoryDB { get; }
+
+        ISQLitePlatform Platforma { get; }
+
+    }
+}
